3ffd3acd5b7e0f23afbab052e92a9ee340b1edb7
ryhornalehach <ryhornalehach@icloud.com>
2017-07-20 21:49:03 -0400

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+Developed by: Ryhor Nalehach
---
+++ react/src/containers/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <h3>Hi from React!</h3>
+      </div>
+    );
+  };
+};
+
+export default App;
