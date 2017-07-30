6bc0be8a610da537b227c3a950f8ae88bc67042e
ryhornalehach <ryhornalehach@icloud.com>
2017-07-30 16:20:14 -0400

    added driver's cabinet

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "users/registrations" }
+  resources :users, only: [:show]
   root 'static_pages#homepage'
 end
