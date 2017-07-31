593c7cb480bbbad8b4600eb860be796d52ca7dfd
ryhornalehach <ryhornalehach@icloud.com>
2017-07-31 14:24:39 -0400

    showing only assigned clients in the index

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::UsersController < ApplicationController
   def index
     if current_user
-      render json: { auth: true }
+      render json: { auth: true, id: current_user.id }
     else
-      render json: { auth: false }
+      render json: { auth: false, id: nil }
     end
   end
 end
