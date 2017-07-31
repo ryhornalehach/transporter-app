904ba431c32b6cb92b09fd7c0e2ce5480cb3ed34
ryhornalehach <ryhornalehach@icloud.com>
2017-07-31 11:05:36 -0400

    added 'my clients' link to the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "users/registrations" }
   resources :users, only: [:index, :show, :destroy, :edit, :update]
   root 'static_pages#homepage'
+  resources :pickups, only: [:index, :show], to: 'static_pages#index'
 end
