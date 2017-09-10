fbc25bba42125cf279c377afc690866bdd5ab683
ryhornalehach <ryhornalehach@icloud.com>
2017-09-10 16:38:24 -0400

    days-records

--- react/src/containers/Day.js
+++ react/src/containers/Day.js
@@ -29,7 +29,7 @@ class Day extends Component {
       dayTile = new Date (this.state.day.date);
       records = this.state.records.map( (record, index) => {
         return(
-          `record # ${index}; client id = ${record.pickup1_id}`
+          `record # ${index}; client id = ${record.pickup1_id} ; `
         )
       })
     } else {
@@ -41,6 +41,60 @@ class Day extends Component {
         <div className="red-text">{this.state.error}</div>
         {dayTile.toDateString()}
         <p>{records}</p>
+
+        <table className="bordered">
+          <thead>
+            <tr>
+                <th>Order#</th>
+                <th>Pickup Time</th>
+                <th>Appointment Time</th>
+                <th>Comments</th>
+                <th>Name</th>
+                <th>Pickup Address</th>
+                <th>Pickup City</th>
+                <th>Dropoff Address</th>
+                <th>Dropoff City</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>6.00</td>
+              <td>6.45</td>
+              <td>Back door</td>
+              <td>Arnold Schwarzenegger</td>
+              <td>88 Main st.</td>
+              <td>Fitchburg</td>
+              <td>380 Brookline ave.</td>
+              <td>Boston</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>7.00</td>
+              <td>7.45</td>
+              <td>Call before</td>
+              <td>Bruce Willis</td>
+              <td>11 Summer ave</td>
+              <td>Everett</td>
+              <td>88 E.Newton st.</td>
+              <td>Boston</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>7.30</td>
+              <td>8.30</td>
+              <td>Service dog</td>
+              <td>Chuck Norris</td>
+              <td>248 Galvin st.</td>
+              <td>Revere</td>
+              <td>77 Bridge ave.</td>
+              <td>Lynn</td>
+            </tr>
+          </tbody>
+        </table>
+
+
       </div>
     )
   }
