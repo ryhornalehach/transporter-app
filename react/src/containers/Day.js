37926ee1df1b98491873ae8f7d75eaab264bc62c
ryhornalehach <ryhornalehach@icloud.com>
2017-09-13 15:38:41 -0400

    debug

--- react/src/containers/Day.js
+++ react/src/containers/Day.js
@@ -27,20 +27,27 @@ class Day extends Component {
   }
   render() {
     let dayTile, records, currentDriverName;
+    console.log('step1');
     if (!this.state.error) {
       dayTile = new Date (this.state.day.date);
+      console.log('step2');
       records = this.state.records.map( (record, index) => {
+        console.log('step records map');
         let currentClientsGroup = [];
         this.state.drivers.forEach((driver) =>{
           if (driver.id === record.driver_id ) {
             currentDriverName = `${driver.first_name} ${driver.last_name}`
+            console.log('step driver name');
           }
         })
         this.state.pickups.forEach((pickup) => {
+          console.log('step5');
           if (record.pickup1_id === pickup.id || record.pickup2_id === pickup.id || record.pickup3_id === pickup.id) {
             currentClientsGroup.push(pickup)
+            console.log('step group');
           }
         })
+        console.log('step return');
         return(
           <RecordTile
             key={index}
@@ -51,6 +58,7 @@ class Day extends Component {
         )
       })
     } else {
+      console.log('step else');
       dayTile = 'You are not authorized';
     }
 
