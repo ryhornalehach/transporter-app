91af0195b37b632db7232bfa97121a9eb1e8fa3a
ryhornalehach <ryhornalehach@icloud.com>
2017-09-19 16:21:08 -0400

    Assigning drivers

--- app/controllers/api/v1/records_controller.rb
+++ app/controllers/api/v1/records_controller.rb
@@ -5,18 +5,34 @@ class Api::V1::RecordsController < ApplicationController
     if current_user
       if current_user.role === 'admin' || current_user.role === 'manager'
         data = JSON.parse(request.body.read)
-        selectedOrder = data['selectedOrder'].to_i
-        if selectedOrder > 0
-          new_order = selectedOrder
-        elsif selectedOrder == 0
-          new_order = nil
-        else
-          new_order = nil
+        if !data['selectedOrder'].nil?
+          selectedOrder = data['selectedOrder'].to_i
+          if selectedOrder > 0
+            new_order = selectedOrder
+          elsif selectedOrder == 0
+            new_order = nil
+          else
+            new_order = nil
+          end
+          record = Record.find(data['currentRecordId'])
+          record.order = new_order
+          record.save
+          render json: { record_order: record.order, error: nil }
         end
-        record = Record.find(data['currentRecordId'])
-        record.order = new_order
-        record.save
-        render json: { record_order: record.order, error: nil }
+        if !data['selectedDriverId'].nil?
+          selectedDriverId = data['selectedDriverId'].to_i
+          if selectedDriverId > 0
+            new_driver = selectedDriverId
+          elsif selectedDriverId == 0
+            new_driver = nil
+          else
+            new_driver = nil
+          end
+          record = Record.find(data['currentRecordId'])
+          record.driver_id = new_driver
+          record.save
+          render json: { record_driver: record.driver_id, error: nil }
+        end
       else
         render json: { error: 'You are not authorized' }
       end
