d9cb1bf28816a0969fffe8f9b8d83d00e2a47bcc
ryhornalehach <ryhornalehach@icloud.com>
2017-09-21 17:27:21 -0400

    sorting in day show page

--- react/src/containers/Day.js
+++ react/src/containers/Day.js
@@ -71,65 +71,72 @@ class Day extends Component {
   }
 
   render() {
-    let dayTile, records, currentDriverId;
+    let dayTile, records, currentDriverId, driversRecordsArranged;
     if (!this.state.error) {
       dayTile = new Date (this.state.day.date);
-      records = this.state.records.map( (record, index) => {
-        let currentClientsGroup = [];
-        let cancelButton = this.state.cancelButton;
-        let confirmButton = this.state.confirmButton;
-        let groupFormText = this.state.groupFormText;
-        let groupFormColor = this.state.groupFormColor;
-        if (record.driver_id > 0) {
-          currentDriverId = record.driver_id;
-        } else {
-          currentDriverId = 0;
-        }
 
-        if (this.state.newGroupRecordId == record.id) {
-          groupFormText = '1st';
-          groupFormColor = 'amber lighten-2';
-          cancelButton = true;
-        } else if (this.state.newGroupPickup2 == record.pickup1_id) {
-          groupFormText = '2nd';
-          groupFormColor = 'amber';
-          confirmButton = true;
-        } else if (this.state.newGroupPickup3 == record.pickup1_id) {
-          groupFormText = '3nd';
-          groupFormColor = 'amber';
-          confirmButton = true;
-        } else if (record.pickup2_id) {
-          groupFormText = 'Split';
-          groupFormColor = 'red accent-1';
-        } else if (this.state.newGroupPickup3 !== null) {
-          groupFormText = '--';
-        } else if (this.state.newGroupPickup2 !== null) {
-          groupFormText = '3rd';
-        } else if (this.state.newGroupRecordId !== null) {
-          groupFormText = '2nd';
-        }
-
-        this.state.pickups.forEach((pickup) => {
-          if (record.pickup1_id === pickup.id || record.pickup2_id === pickup.id || record.pickup3_id === pickup.id) {
-            currentClientsGroup.push(pickup)
+      // driversRecordsArranged = this.state.drivers.forEach((driver) => {
+        records = this.state.records.map( (record, index) => {
+          let currentClientsGroup = [];
+          let cancelButton = this.state.cancelButton;
+          let confirmButton = this.state.confirmButton;
+          let groupFormText = this.state.groupFormText;
+          let groupFormColor = this.state.groupFormColor;
+          if (record.driver_id > 0) {
+            currentDriverId = record.driver_id;
+          } else {
+            currentDriverId = 0;
           }
+
+          if (this.state.newGroupRecordId == record.id) {
+            groupFormText = '1st';
+            groupFormColor = 'amber lighten-2';
+            cancelButton = true;
+          } else if (this.state.newGroupPickup2 == record.pickup1_id) {
+            groupFormText = '2nd';
+            groupFormColor = 'amber';
+            confirmButton = true;
+          } else if (this.state.newGroupPickup3 == record.pickup1_id) {
+            groupFormText = '3nd';
+            groupFormColor = 'amber';
+            confirmButton = true;
+          } else if (record.pickup2_id) {
+            groupFormText = 'Split';
+            groupFormColor = 'red accent-1';
+          } else if (this.state.newGroupPickup3 !== null) {
+            groupFormText = '--';
+          } else if (this.state.newGroupPickup2 !== null) {
+            groupFormText = '3rd';
+          } else if (this.state.newGroupRecordId !== null) {
+            groupFormText = '2nd';
+          }
+
+          this.state.pickups.forEach((pickup) => {
+            if (record.pickup1_id === pickup.id || record.pickup2_id === pickup.id || record.pickup3_id === pickup.id) {
+              currentClientsGroup.push(pickup)
+            }
+          })
+          return(
+            // <div>
+            // {driver.first_name}
+            <RecordTile
+              key={index}
+              record={record}
+              currentClientsGroup={currentClientsGroup}
+              currentDriverId={currentDriverId}
+              makeNewGroup={this.makeNewGroup}
+              onClickGroup={this.onClickGroup}
+              onClickCancel={this.onClickCancel}
+              groupFormText={groupFormText}
+              groupFormColor={groupFormColor}
+              cancelButton={cancelButton}
+              confirmButton={confirmButton}
+            />
+            // </div>
+          )
         })
-        return(
-          <RecordTile
-            key={index}
-            record={record}
-            currentClientsGroup={currentClientsGroup}
-            currentDriverId={currentDriverId}
-            makeNewGroup={this.makeNewGroup}
-            onClickGroup={this.onClickGroup}
-            onClickCancel={this.onClickCancel}
-            groupFormText={groupFormText}
-            groupFormColor={groupFormColor}
-            cancelButton={cancelButton}
-            confirmButton={confirmButton}
-          />
-        )
-      })
+      // })
+
     } else {
       dayTile = 'You are not authorized';
     }
