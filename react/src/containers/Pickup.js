59c219daf8ac553b3c7282477031f2227e33f1b2
ryhornalehach <ryhornalehach@icloud.com>
2017-09-27 10:51:11 -0400

    modified pickup show page

--- react/src/containers/Pickup.js
+++ react/src/containers/Pickup.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PickupTile from '../components/PickupTile';
 import AdminPickupTile from '../components/AdminPickupTile';
-import AssignForm from '../components/AssignForm';
 import MapComponent from '../components/MapComponent';
 
 class Pickup extends Component {
@@ -15,15 +14,10 @@ class Pickup extends Component {
       pickedUp: false,
       droppedOff: false,
       showUser: false,
-      allDrivers: [],
-      assignedDriverText: 'N/A',
-      selectedDriverId: 0,
       error: null
     }
     this.handlePickupButton = this.handlePickupButton.bind(this);
     this.handleDropoffButton = this.handleDropoffButton.bind(this);
-    this.handleForm = this.handleForm.bind(this);
-    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -34,12 +28,7 @@ class Pickup extends Component {
     })
     .then(response => response.json())
     .then(pickup => {
-      if (pickup.driver) {
-        let name = `${pickup.driver.first_name} ${pickup.driver.last_name}`
-        this.setState({ pickupInfo: pickup.pickup, pickupId: pickupId, pickedUp: pickup.pickup.picked_up, droppedOff: pickup.pickup.dropped_off, assignedDriverText: name })
-      } else {
-        this.setState({ pickupInfo: pickup.pickup, pickupId: pickupId, pickedUp: pickup.pickup.picked_up, droppedOff: pickup.pickup.dropped_off })
-      }
+      this.setState({ pickupInfo: pickup.pickup, pickupId: pickupId, pickedUp: pickup.pickup.picked_up, droppedOff: pickup.pickup.dropped_off })
     })
 
     fetch('/api/v1/users',{
@@ -47,37 +36,7 @@ class Pickup extends Component {
     })
     .then(response => response.json())
     .then(body => {
-      if (body.allDrivers) {
-        let allDriversList = body.allDrivers
-        let empty = { id: null, first_name: '--', last_name: '--' }
-        allDriversList.unshift(empty);
-        this.setState({ allDrivers: allDriversList, showUser: body.auth, currentUser: body.user })
-      } else {
-        this.setState({ showUser: body.auth, currentUser: body.user })
-      }
-    })
-  }
-
-  handleChange(event) {
-    this.setState({ selectedDriverId: event.target.value})
-  }
-
-  handleForm(event) {
-    event.preventDefault();
-    fetch(`/api/v1/users/${this.state.selectedDriverId}/`, {
-      method: 'PATCH',
-      credentials: "same-origin",
-      body: JSON.stringify({ selectedDriverId: this.state.selectedDriverId, currentClientId: this.state.pickupId })
-    })
-    .then(response => response.json())
-    .then(body => {
-      let name;
-      if (body) {
-        name = `${body.first_name} ${body.last_name}`;
-      } else {
-        name = 'N/A';
-      }
-      this.setState({ assignedDriverText: name })
+      this.setState({ showUser: body.auth, currentUser: body.user })
     })
   }
 
@@ -112,7 +71,7 @@ class Pickup extends Component {
   }
 
   render() {
-    let buttonConfirmPickup, form, buttonConfirmDropoff, pickupTile, origin, destination;
+    let buttonConfirmPickup, buttonConfirmDropoff, pickupTile, origin, destination;
 
     if (this.state.droppedOff){
       buttonConfirmDropoff = <button type="button" className="btn waves-effect waves-light red" onClick={this.handleDropoffButton}>Not dropped off yet?</button>
@@ -150,18 +109,6 @@ class Pickup extends Component {
                         handleDropoffButton={this.handleDropoffButton}
                   />
     }
-    if (this.state.currentUser.admin) {
-
-      form = <AssignForm
-                  allDrivers={this.state.allDrivers}
-                  currentClientId={this.state.pickupId}
-                  handleForm={this.handleForm}
-                  handleChange={this.handleChange}
-                  selectedDriverId={this.state.selectedDriverId}
-              />
-    } else {
-      form = null;
-    }
 
     origin = `${this.state.pickupInfo.pickup_address}, ${this.state.pickupInfo.pickup_city}`
     destination = `${this.state.pickupInfo.dropoff_address}, ${this.state.pickupInfo.dropoff_city}`
@@ -173,10 +120,6 @@ class Pickup extends Component {
         <div className="row">
           <div className="col s12 l6">
             <Link to='/pickups' className="btn waves-effect waves-light navbar-color-dark">Back to all clients</Link>
-          </div>
-          <div className="col s12 l6">
-            <div className="large-text"><b>Assigned driver: </b>{this.state.assignedDriverText}</div>
-            {form}
           </div>
           <div className="row">
             <div className="col s12">
