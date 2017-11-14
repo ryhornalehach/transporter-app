785f47d536f8e425b25290e6b78a3fd3b08e9269
ryhornalehach <ryhornalehach@icloud.com>
2017-11-14 11:45:33 -0500

    google sheets gem

--- app/controllers/api/v1/days_controller.rb
+++ app/controllers/api/v1/days_controller.rb
@@ -1,7 +1,55 @@
+require_relative "../../../../quickstart.rb"
+
 class Api::V1::DaysController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  def authorize
+    FileUtils.mkdir_p(File.dirname(CREDENTIALS_PATH))
+
+    client_id = Google::Auth::ClientId.from_file(CLIENT_SECRETS_PATH)
+    token_store = Google::Auth::Stores::FileTokenStore.new(file: CREDENTIALS_PATH)
+    authorizer = Google::Auth::UserAuthorizer.new(
+      client_id, SCOPE, token_store)
+    user_id = 'default'
+    credentials = authorizer.get_credentials(user_id)
+    if credentials.nil?
+      url = authorizer.get_authorization_url(
+        base_url: OOB_URI)
+      puts "Open the following URL in the browser and enter the " +
+           "resulting code after authorization"
+      puts url
+      code = gets
+      credentials = authorizer.get_and_store_credentials_from_code(
+        user_id: user_id, code: code, base_url: OOB_URI)
+    end
+    credentials
+  end
+
   def index
+
+    # Initialize the API
+    service = Google::Apis::SheetsV4::SheetsService.new
+    service.client_options.application_name = APPLICATION_NAME
+    service.authorization = authorize
+
+    # Prints the names and majors of students in a sample spreadsheet:
+    # https://docs.google.com/spreadsheets/d/1FCSYucWISilyzlLif7hwFsARVtn91k2hkxLNaaX18KY/edit
+    spreadsheet_id = '1FCSYucWISilyzlLif7hwFsARVtn91k2hkxLNaaX18KY'
+    range = 'Schedule!A4:L'
+    puts 'HI there'
+    response = service.get_spreadsheet_values(spreadsheet_id, range)
+    puts 'Name, Phone:'
+    puts 'No data found.' if response.values.empty?
+      # Displaying only Timothy`s clients
+    response.values.each do |row|
+      if row[1] == "Timothy"
+        puts "#{row[6]}, #{row[7]}"
+      end
+      # Print columns G and H, which correspond to indices 6 and 7.
+      # puts "#{row[6]}, #{row[7]}"
+    end
+
+    binding.pry
     if current_user
       if current_user.role === 'admin' || current_user.role === 'manager'
         allDays = Day.all.reverse
